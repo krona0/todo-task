dd8451d5dcb1a6dd283c42ba7627acad25d40143
krona0 <158546309+krona0@users.noreply.github.com>
2024-02-26 04:37:51 +0800

    Update te.py
    一个批量创建文件夹的程序

--- pobj/te.py
+++ pobj/te.py
@@ -1,1 +1,24 @@
+import os
 
+def cr_dir(a):
+    for i in range(1,int(a) + 1):
+        os.makedirs('text' + str(i))
+#        print(i)
+
+def ls_dir():
+    for i in list:
+        print(str(i))
+
+print('当前目录: ' + os.getcwd())
+fmnu = input('你想创建多少文件夹: ')
+print(fmnu)
+cr_dir(fmnu)
+print('已为你创建' + str(fmnu) + '文件夹')
+list = os.listdir('./')
+#print(list[0])
+print('当前目录文件为: ')
+ls_dir()
+
+
+#os.makedirs('test')
+#print(os.getcwd())
